496f8e755cd1fe4959ae2b81c210a1e83190f111
dinesh0085 <105920688+dinesh0085@users.noreply.github.com>
2023-01-19 13:23:41 +0530

    added backend

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express=require('express')
+require("dotenv").config();
+const connect=require("./Database/dbConnect")
+const userRouter=require("./route/user.route")
+
+const app = express()
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+app.get('/',(req,res)=>{
+ return res.send('Hello')
+})
+
+app.use("/user",userRouter)
+
+app.listen(8080,async()=>{ 
+  try{
+   await connect()
+   console.log("Database Connected");
+  }catch(e){
+   console.log(e);
+  }
+    console.log('Listening Server to Port 8080')
+})
---
+++ src/route/user.route.js
@@ -0,0 +1,50 @@
+const express=require("express");
+const userModel=require("../model/user.model");
+const bcrypt = require('bcrypt');
+const jwt=require("jsonwebtoken");
+
+const app=express.Router();
+
+app.post("/signup",async(req,res)=>{
+    const {email,password} = req.body;
+
+    
+     try{
+        // let hashed;
+        //  hashed=await bcrypt.hash(password, 10,async function(err, hash) {
+        //    return hash
+        // });
+        // console.log(hashed);
+        const user=new userModel({email,password});
+        await user.save()
+        res.status(201).send({msg:"User created Successfully"})
+     }catch(e){
+        res.send({msg:e.message})
+     }
+    
+      
+
+})
+
+app.post("/login",async(req,res)=>{
+    const {email,password} = req.body;
+
+   try{
+     const user=await userModel.findOne({email,password});
+     console.log(user);
+     if(user){
+        const token=jwt.sign({id:user._id,email:user.email},"SECRETKEY",
+        {
+            expiresIn:"1 day"
+        })
+       res.status(201).send({msg:"Login Successful",token})
+     }else{
+        res.status(201).send({msg:"Invalid Credentials"})
+     }
+    }catch(e){
+     res.status(400).send({msg:e.message})
+    }
+})
+
+
+module.exports=app
