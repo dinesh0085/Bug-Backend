e62e39fe6ae88996571d79099e009937dd96cffb
dinesh0085 <105920688+dinesh0085@users.noreply.github.com>
2023-01-19 14:46:18 +0530

    added backend

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ const express=require('express')
 require("dotenv").config();
 const connect=require("./Database/dbConnect")
 const userRouter=require("./route/user.route")
+const cors=require("cors")
 
 const app = express()
 
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
+app.use(cors())
 
 app.get('/',(req,res)=>{
  return res.send('Hello')
